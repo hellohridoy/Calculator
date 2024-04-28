70f73f4a790b41e67be96f7a749c5f0a5d3ae21e
Hridoy <Hridoy>
2024-04-28 09:08:43 +0600

    java basic

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        int numberOne = 1056678701;
+        if(numberOne%2==0){
+            System.out.println("This is even number");
+        } else {
+            System.out.println("This is odd number");
+        }
+    }
+}
